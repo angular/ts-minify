06465c5af094c78798d51da99b7a4566934800c6
Daria <djj2115cu@gmail.com>
2015-06-26 13:34:08 -0700

    test(minifier): Add gulp task for testing
    Tests written in TypeScript are transpiled into JavaScript and run via gulp. Unit test for getNextLateralPropertyName passes.

--- test/e2e/test.js
+++
@@ -1,6 +0,0 @@
-var assert = require('assert')
-describe('Hello World', function(){
-	it('The first char of "Hello World" should be "H"', function(){
-		assert.equal('H', "Hello World"[0]);
-	})
-})
