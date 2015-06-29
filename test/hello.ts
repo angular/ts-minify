f065b9bb4b3765789519925919c79b7e1d3ca26f
Daria <djj2115cu@gmail.com>
2015-06-29 16:56:37 -0700

    feat(minifier): Implement assignNewPropertyName method
    Writes the logic for checking whether a property name exists in the rename map, and if not, adding it with the new generated name.

--- test/hello.ts
+++ test/hello.ts
@@ -1,11 +1,21 @@
 class Greeter {
-  constructor(public greeting: string, public name: string) { 
-  }
-  greet() {
-    return "<h1>" + this.name + "says: " + this.greeting + "</h1>";
-  }
+	greeting: string;
+	name: string;
+
+	constructor(public greeting: string, public name: string) { 
+		this.greeting = greeting;
+		this.name = name;
+	}
+	greet() {
+		return "<h1>" + this.name + "says: " + this.greeting + "</h1>";
+	}
 };
 var greeter: Greeter = new Greeter("Hello, world!", "daria");
 var str = greeter.greet();
-document.body.innerHTML = str;
 
+document.body.innerHTML.fontsize = 7;
+
+var hello;
+
+
+// document . body . innerHTML 
