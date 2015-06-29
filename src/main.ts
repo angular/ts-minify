fe998758e430bbbbf45c2907a9895981ee15e581
Daria <djj2115cu@gmail.com>
2015-06-29 16:56:37 -0700

    feat(minifier): Implement assignNewPropertyName method
    Writes the logic for checking whether a property name exists in the rename map, and if not, adding it with the new generated name.

--- src/main.ts
+++ src/main.ts
@@ -5,29 +5,54 @@ import ts = require('typescript');
 var fs = require('fs'); // filesystem module
 var util = require('util');
 var path = require('path');
-var Map = require('es6-map');
 
 /* TranspilerOptions Class will go here */
 
+class PropertyInfo {
+  private lhs: string;
+  private newName: string;
+
+  constructor(lhs: string, newName: string) {
+    this.lhs = lhs;
+    this.newName = newName;
+  }
+
+  getLHS(): string {
+    return this.lhs;
+  }
+
+  getNewName(): string {
+    return this.newName;
+  }
+}
+
 /* The Transpiler Class */
-export class Transpiler {
+export class Renamer {
   private output: string; // for now, what is an output object?
   private currentFile: ts.SourceFile;
 
-  // initialize to ''
-  lastRename: string = '';
+  /* 
+   * List of properties, and their remappings. Property has what type its parent is (left hand side
+   * expression) and its new name. 
+   */
+  renameMap = new Map();
+  /* 
+   * List of "root" names, and the last property name assigned to one of its properties.
+   * ie: Foo is a class. Foo has properties x, y, z which were renamed to a, b, c. 
+   * { Foo: 'c' } allows easy access to Foo's last renamed property so we can use 
+   * getNextLateralPropertyName per Class, not globally. 
+   */
+  prevNameMap = new Map();
 
-  renameMap = new Map(); // ???
+  /* Not sure if needed */
   nodes: ts.Node[] = [];
 
-  // last comment index?
+  /* Not needed as of now */
   private errors: string[] = [];
 
-  //private transpilers;
-  // (Transpiler options here when I know what's needed) 
 
   constructor() {
-    // will instantiate different transpilers; nothing here yet
+    /* nothing here yet */
   } 
 
   compile(fileNames: string[], options: ts.CompilerOptions): void {
@@ -57,7 +82,7 @@ export class Transpiler {
     });
   }
 
-  // return set options for the compiler
+  /* return set options for the compiler */
   getCompilerOptions(): ts.CompilerOptions {
     const options: ts.CompilerOptions = {
       allowNonTsExtensions: true,
@@ -79,7 +104,8 @@ export class Transpiler {
     //      console.log(exists ? "exists" : "nope :(");
     //  });
     // });
-    // the methods of a compiler host object
+
+    /* the methods of a compiler host object */
     return {
       getSourceFile: (sourceName, languageVersion) => {
         if (fileMap.hasOwnProperty(sourceName)) {
@@ -108,88 +134,108 @@ export class Transpiler {
     };
   }
 
-  /* For later? Do I even need this though. */
-  prettyPrint() {
-  }
-
   /* Walk the AST of the program */
-  walk(sourcefile: ts.SourceFile, program: ts.Program) {
-    var typeChecker = program.getTypeChecker();
+  /* Pass in typechecker instead of program */
+  walk(sourcefile: ts.SourceFile, typeChecker: ts.TypeChecker) {
+    var map = this.renameMap;
 
-    //console.log(typeChecker.getTypeAtLocation(sourcefile));
-    var _this = this;
-    traverse(sourcefile, typeChecker, _this.renameMap);
+    ts.forEachChild(sourcefile, function(node) {
+      traverse(node, typeChecker, map, '');
+    });
 
-    function traverse(node: ts.Node, typeChecker, renameMap, count?: number) {
+    /* Somewhat of a misnomer to refer to pString as "parent" */
+    function traverse(node: ts.Node, typeChecker: ts.TypeChecker, renameMap, pString: string) {
       switch (node.kind) {
-        case ts.SyntaxKind.PropertyAssignment:
-          console.log('PropertyAssignment');
+        case ts.SyntaxKind.ClassDeclaration:
+          var classDeclaration = <ts.ClassDeclaration>node;
+          //parent.push(classDeclaration.name.text);
+          pString = updateParentString(pString, classDeclaration.name.text);
+          console.log('ClassDeclaration');
+          console.log(classDeclaration);
+          //console.log(classDeclaration.members);
+
+          // for (var mem in classDeclaration.members) {
+          //   var member = classDeclaration.members[mem];
+          //   console.log(member);
+          //   console.log("member.kind " + member.kind);
+          // }
           break;
+        case ts.SyntaxKind.PropertyAssignment:
+          break;
+        /* This is where we rename the property and add it to the dictionary */
         case ts.SyntaxKind.PropertyDeclaration:
-          console.log('PropertyDeclaration');
+          console.log('PropertyDeclaration ' + 'parent: ' + pString);
+          var pd = <ts.PropertyDeclaration>node;
+          pString = updateParentString(pString, pd.name.text);
+          console.log("type ");
+          getType(pd.type);
+
+          //console.log("TODO ADD TO RENAME MAP: " + pString);
+          //renameMap.set(pString, "NOTHING YET");
+          break;
+        case ts.SyntaxKind.Constructor:
+          break;
+        case ts.SyntaxKind.MethodDeclaration:
           break;
         case ts.SyntaxKind.ShorthandPropertyAssignment:
-          console.log('ShorthandPropertyAssignment');
           break;
         case ts.SyntaxKind.BinaryExpression:
-          //console.log('BinaryExpression');
-          var binExpr = <ts.BinaryExpression>node;
-          //console.log(binExpr);
           break;
         case ts.SyntaxKind.Identifier:
-          var ident = <ts.Identifier>node;
-          //console.log(typeChecker.getTypeAtLocation(ident));
           break;
         case ts.SyntaxKind.DotToken:
-          console.log("dot token, do nothing");
           break;
-        // PAE has: 
-        // 1. expression: LeftHandSideExpression
-        // 2. dotToken: Node
-        // 3. name: Identifier (right hand side of expression)
         case ts.SyntaxKind.PropertyAccessExpression:
-          var pae = <ts.PropertyAccessExpression>node; // is this casting?
-          console.log('PropertyAccessExpression');
-          console.log("========================================================");
-          /* If _.expression.text exists, then it is a top level "name" */
-          if (pae.expression.text) {
-            console.log("PAE_EXPRESSION_TEXT " + pae.expression.text + ": " + pae.name.text);
-            console.log(pae.expression);
-            renameMap.set(pae.expression.text + '.' + pae.name.text, "nothing for now");
-          }   
+          var pae = <ts.PropertyAccessExpression>node;
+          var lhs = pae.expression;
 
-          // "property" name
-          // if (pae.name) {
-          //  console.log("PAE_EXPRESSION_NAME ");
-          //  console.log(pae.name);
-          // }
-          // console.log(pae.expression);
-          // console.log(pae.dotToken);
-          // console.log(pae.name);
-
-          //this.renameMap.set(pae);
-
-          //console.log(pae.expression + ": " + typeChecker.typeToString(
-          //  typeChecker.getTypeAtLocation(pae.expression)));
-          //console.log(pae.name + ": " + typeChecker.typeToString(
-          //  typeChecker.getTypeAtLocation(pae.name)));
-
-          //console.log(pae.expression.text); // doesn't have it but it prints? I don't get it.
-
-
-          // this.map.set(pae.expression.text, { });
-          
-          console.log("========================================================");
+          if (lhs.text) {
+            console.log("PropertyAE lhs.text");
+            //parent.push(pae.name.text);
+            //parent.push(lhs.text);
+            pString = updateParentString(lhs.text + '$' + pae.name.text, pString);
+            console.log("TODO ADD TO RENAME MAP: " + pString);
+            //renameMap.set(pString, "NOTHING YET");
+            getType(pae);
+          } else if (lhs.expression) {
+            console.log("PropertyAE lhs.expression");
+            pString = updateParentString(pae.name.text, pString);
+            getType(pae);
+          } else {
+            console.log("PropertyAE else");
+            //parent.push(pae.name.text);
+            pString = updateParentString(pString, pae.name.text);
+            //console.log("TODO ADD TO RENAME MAP: " + pString);
+            //renameMap.set(pString, "NOTHING YET");
+          }
           break;
       }
 
-      renameMap.forEach(function(value, key, map) {
-         console.log("Key: %s, Value: %s", key, value);
-      });
-
       ts.forEachChild(node, function(node) {
-        traverse(node, typeChecker, renameMap, count);
+        traverse(node, typeChecker, renameMap, pString);
       });
+    }
+
+    //printMap(this.renameMap);
+
+    function getType(node: ts.Node): string {
+      // TODO: Add type checking to "top-level" expressions
+      try {
+        console.log("TYPECHECKER");
+        console.log(typeChecker.typeToString(typeChecker.getTypeAtLocation(node)));
+        return typeChecker.typeToString(typeChecker.getTypeAtLocation(node));
+      } catch(error) {
+        console.log("TYPECHECKER ERROR " + error.stack);
+        return "error";
+      }
+    }
+
+    function printMap(map: Map<any, any>) {
+      console.log('============ Rename Map ===============');
+      map.forEach(function(value, key) {
+        console.log(key + " = " + value);
+      }, map);
+      console.log('============ Rename Map ===============');
     }
 
     /* Report information when necessary */
@@ -197,16 +243,52 @@ export class Transpiler {
       var lc = sourcefile.getLineAndCharacterOfPosition(node.getStart());
       console.log('${sourcefile.fileName} (${lc.line + 1},${lc.character + 1}): ${message}');
     }
+
+    /* Concat the 'parent' and 'child' strings */
+    function updateParentString(p: string, c: string): string {
+      if (p.length === 0) {
+        return c;
+      } else if (c.length === 0) {
+        return p;
+      } else {
+        return p + '$' + c;
+      }
+    }
+
+    /* TODO: Get rid of it or figure out if it's needed */
+    function dfs(node) {
+      console.log(node);
+      if (node.expression) {
+        console.log(node.expression);
+        dfs(node.expression);
+      } else {
+        return node;
+      }
+    }
+
+    /* TODO: Get rid of it or figure out if it's needed */
+    function childrenExist(topNode) {
+      var count = 0;
+      ts.forEachChild(topNode, function(node) {
+        if (node) count++;
+      });
+
+      console.log(count);
+
+      return (count > 0);
+    }
   }
 
-  // not sure where this should go, transpiler doesn't really make senes?
+  /* 
+   * Given a char, generate the next character in the alphabet.
+   */
   nextChar(c: string): string {
     return String.fromCharCode(c.charCodeAt(0) + 1);
   }
 
   /* Returns a string for the new property name */
   /* 0 -> a, 1 -> b, ... 25 -> z, 26 -> aa , ...*/
-  generate_next_lateral_property_name(code: string): string {
+  generateNextLateralPropertyName(code: string): string {
     var chars = code.split('');
     console.log(chars);
     var len: number = code.length;
@@ -225,15 +307,54 @@ export class Transpiler {
       }
     }
     return chars.join('');
+  }  
+
+  /* Given a key, assign a generated property shortname */
+  assignNewPropertyName(propName: string, lhsType: string): void {
+    var prevRename = this.getLastRename(lhsType);
+    var newPropName = this.generateNextLateralPropertyName(prevRename);
+    var value = new PropertyInfo(lhsType, newPropName);
+    /* 
+     * Add the PropertyInfo for the property name in a new list since this property name might
+     * not be unique.
+     */
+    if (!this.renameMap.get(propName)) {
+      this.renameMap.set(propName, [value]);
+    } else {
+      var arr = this.renameMap.get(propName);
+      this.renameMap.set(propName, arr.push(value));
+    }
+    this.updateLastRename(lhsType, newPropName);
   }
 
+  /* Update the last renamed property for the lhs expression */
+  updateLastRename(key: string, rename: string): void {
+    this.prevNameMap.set(key, rename);
+  }
+
+  getLastRename(key: string): string {
+    /*
+     * This LHS expression does not exist yet. Add it to the prevNameMap.
+     */
+    if (!this.prevNameMap.get(key)) {
+      /* Set initial last property name to '' so 'a' can be generated correctly since
+       * generateNextLateralPropertyName is based on the most recently generated name.
+       */
+      this.prevNameMap.set(key, '');
+      return '';
+    } else {
+      return this.prevNameMap.get(key);
+    }
+  }
 }
 
-//var transpiler = new Transpiler();
-//var host = transpiler.createCompilerHost(['../../test/hello.ts']);
+var renamer = new Renamer();
+var host = renamer.createCompilerHost(['../../test/hello.ts']);
 //console.log('created compiler host');
-//var source : ts.SourceFile = host.getSourceFile('../../test/hello.ts', ts.ScriptTarget.ES6);
+var source : ts.SourceFile = host.getSourceFile('../../test/hello.ts', ts.ScriptTarget.ES6);
 
-// to create the program, the host calls getSourceFile IF you pass in a host. It's an optional parameter
-//var program : ts.Program = ts.createProgram(['../../test/hello.ts'], transpiler.getCompilerOptions());
-//transpiler.walk(source, program);
+// to create the program, the host calls getSourceFile 
+// IF you pass in a host. It's an optional parameter
+var program : ts.Program = 
+  ts.createProgram(['../../test/hello.ts'], renamer.getCompilerOptions());
+renamer.walk(source, program.getTypeChecker());
