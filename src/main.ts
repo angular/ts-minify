7b2552f9950c6188dceed49d551a529ebefccb5f
Daria <djj2115cu@gmail.com>
2015-07-21 17:32:31 -0700

    feat(minifier): structural setup
    Sets up gulp tasks (build process), testing framework, initial Minifier class.

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+/// <reference path='../node_modules/typescript/bin/typescript.d.ts' />
+
+import * as ts from 'typescript';
+
+class Transpiler {
+  constructor() {}
+
+  helloWorld() { return 'hello world'; }
+}
