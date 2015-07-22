3c922a233764f3dbeb36b9e768847374adbab417
Daria <djj2115cu@gmail.com>
2015-07-21 17:32:31 -0700

    structural setup

---
+++ test/unit/main_test.ts
@@ -0,0 +1,8 @@
+/// <reference path="../../typings/mocha/mocha.d.ts"/>
+/// <reference path="../../typings/chai/chai.d.ts"/>
+
+import * as assert from 'assert';
+
+describe('Equality statement', function() {
+  it('shows that 1 equals 1', function() { assert.equal(1, 1); })
+});
