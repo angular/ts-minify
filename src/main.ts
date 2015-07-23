c70fc4c94feda954e4e510bee46b10319e2eafbf
Daria <djj2115cu@gmail.com>
2015-07-23 16:24:39 -0700

    feat(minifier): Add error checking on TypeScript input
    Throws Malformed TypeScript error on invalid TypeScript input.

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,34 @@
 
 import * as ts from 'typescript';
 
-class Transpiler {
+const DEBUG = false;
+
+export const options: ts.CompilerOptions = {
+  allowNonTsExtensions: true,
+  module: ts.ModuleKind.CommonJS,
+  target: ts.ScriptTarget.ES5,
+};
+
+export class Minifier {
   constructor() {}
 
-  helloWorld() { return 'hello world'; }
+  checkForErrors(program: ts.Program) {
+    var errors = [];
+    var emitResult = program.emit();
+    var allDiagnostics = ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics);
+
+    allDiagnostics.forEach(diagnostic => {
+      if (diagnostic.file && !diagnostic.file.fileName.match(/\.d\.ts$/)) {
+        var {line, character} = diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start);
+        var message = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
+        errors.push(`${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`);
+      }
+    });
+
+    if (errors.length > 0) {
+      throw new Error(
+          'Malformed TypeScript: Please check your source-files before attempting to use ts-minify.\n' +
+          errors.join('\n'));
+    }
+  }
 }
