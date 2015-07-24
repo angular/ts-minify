a9dc8bcc77b73db5cc8ae9d93ce19f673e7dc82b
Daria <djj2115cu@gmail.com>
2015-07-23 17:32:09 -0700

    feat(minifier): Implement a visitor pattern for the AST
    Visit nodes to create new output with renaming ('$mangled').

--- test/unit/main_test.ts
+++ test/unit/main_test.ts
@@ -1,14 +1,21 @@
 /// <reference path='../../typings/mocha/mocha.d.ts'/>
 /// <reference path='../../typings/chai/chai.d.ts'/>
+/// <reference path='../../typings/node/node.d.ts'/>
 
 import * as assert from 'assert';
-import {Minifier, options} from '../../src/main';
-import * as chai from 'chai';
 import * as ts from 'typescript';
+import * as chai from 'chai';
+import {Minifier, options} from '../../src/main';
+
+function expectTranslate(code: string) {
+  var result = translateSource(code);
+  return chai.expect(result);
+}
 
 function parseFile(fileName: string, fileContent: string): ts.Program {
   var defaultLibName = ts.getDefaultLibFilePath(options);
   var compilerHost: ts.CompilerHost = {
+
     getSourceFile: function(sourceName, languageVersion) {
       return ts.createSourceFile(sourceName, fileContent, options.target, true);
     },
@@ -26,6 +33,21 @@ function parseFile(fileName: string, fileContent: string): ts.Program {
   return program;
 }
 
+function translateSource(content: string): string {
+  var minifier = new Minifier();
+  var program = parseFile('test.ts', content);
+  var sourceFiles = program.getSourceFiles();
+  var namesToContents = {};
+
+  sourceFiles.forEach((sf) => {
+    // if (not a .d.ts file) and (is a .js or .ts file)
+    if (!sf.fileName.match(/\.d\.ts$/) && !!sf.fileName.match(/\.[jt]s$/)) {
+      namesToContents[sf.fileName] = minifier.visit(sf);
+    }
+  });
+  return namesToContents['test.ts'];
+}
+
 describe('Equality statement', () => {it('shows that 1 equals 1', () => { assert.equal(1, 1); })});
 
 describe('Recognizes invalid TypeScript inputs', () => {
@@ -39,4 +61,15 @@ describe('Recognizes invalid TypeScript inputs', () => {
     var program = parseFile('test.ts', '(function blah() {})');
     chai.expect(() => minifer.checkForErrors(program)).to.not.throw();
   })
+});
+
+describe('Visitor pattern', () => {
+  it('shows correctly appends "$mangled" to an identifier of a property access expression', () => {
+    expectTranslate('Math.random();').to.equal('Math.random$mangled();');
+    expectTranslate('Class Foo { a: string; constructor() {} b() { this.a = "hello"; } }')
+        .to.equal('Class Foo { a: string; constructor() {} b() { this.a$mangled = "hello"; } }');
+    expectTranslate('for (x in foo.bar) { var y = foo.bar.baz; }')
+        .to.equal('for (x in foo.bar$mangled) { var y = foo.bar$mangled.baz$mangled; }');
+    expectTranslate('var x = foo.baz();').to.equal('var x = foo.baz$mangled();');
+  });
 });
